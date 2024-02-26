1781a7bc8fb76f33114d541699b5cd8deb11ca27
dylanljs04 <83709962+dylanljs04@users.noreply.github.com>
2024-02-26 13:01:06 +0000

    Add files via upload

---
+++ Inverse Kinematics MATLAB.m
@@ -0,0 +1,23 @@
+% Finding the angles for robot
+
+% Set Parameters
+x = 90;
+y = -60;
+z = 97;
+L1 = 90;
+L2 = 97;
+
+% Calculate theta3
+theta3 = acos((x^2+y^2+z^2-L1^2-L2^2)/(2*L1*L2))*(180/pi);
+
+% Calculate theta2
+phi = atan(z/sqrt(x^2+y^2))*(180/pi);
+beta = (acos((L1^2 + x^2 + y^2 + z^2 - L2^2)/(2*L1*sqrt(x^2 +y^2 +z^2))))*(180/pi);
+theta2 = phi + beta;
+
+% Calculate theta1
+theta1 = acos(y/sqrt(x^2 + y^2))*(180/pi);
+
+
+
+
